d1a358a4e1aba2ab488cdd3c493aefe852db1a5d
Yulia Tarima <yulia.tarima@gmail.com>
2020-03-17 15:56:27 -0700

    Updates

---
+++ precache-manifest.51a29282fdcded378f25679766891b72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a5736ce7a3262d622975051f7d0beb1",
+    "url": "/ReactJS-StarWarsAPI-Website/index.html"
+  },
+  {
+    "revision": "4f58243f9783cdba4a72",
+    "url": "/ReactJS-StarWarsAPI-Website/static/css/main.6d745162.chunk.css"
+  },
+  {
+    "revision": "b8107b2a86a9614f82d7",
+    "url": "/ReactJS-StarWarsAPI-Website/static/js/2.e91f0349.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ReactJS-StarWarsAPI-Website/static/js/2.e91f0349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f58243f9783cdba4a72",
+    "url": "/ReactJS-StarWarsAPI-Website/static/js/main.8e34a849.chunk.js"
+  },
+  {
+    "revision": "6b23eeaad853dea54d85",
+    "url": "/ReactJS-StarWarsAPI-Website/static/js/runtime-main.249acfbe.js"
+  }
+]);
