b6ecfb82f969ab9cd1d18ed707d8cfa305a11cda
Yulia Tarima <yulia.tarima@gmail.com>
2020-03-17 18:09:01 -0700

    Fix: redirect on login.js page

--- src/components/pages/login-page.js
+++ src/components/pages/login-page.js
@@ -4,12 +4,12 @@ import { Redirect } from 'react-router-dom';
 const LoginPage = ({ isLoggedIn, onLogin }) => {
 
     if (isLoggedIn) {
-        return <Redirect to="/"/>;
+        return <Redirect to="/ReactJS-StarWarsAPI-Website/"/>;
     }
 
     return (
         <div className="jumbotron">
-            <p>Login to see secret page!</p>
+            <p>Login to see the secret page!</p>
             <button
                 className="btn btn-primary"
                 onClick={onLogin}>
